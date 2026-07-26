88c73c2b6b50a38c6c365de4c597bddb77ee3e91
koppoju vinay <vinaykoppoju058@gmail.com>
2026-07-26 10:48:40 +0530

    Create student.java

---
+++ student.java
@@ -0,0 +1,30 @@
+package com.student;
+
+public class student {
+	int ID;
+	String name;
+	String course;
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	public int getID() {
+		return ID;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getCourse() {
+		return course;
+	}	
+
+
+}
+
+	
+
