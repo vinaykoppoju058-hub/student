ba7a61c2524b821793f6d9b28fe233f1083a18fd
koppoju vinay <vinaykoppoju058@gmail.com>
2026-07-28 12:01:46 +0530

    Update student.java

--- student.java
+++ student.java
@@ -4,6 +4,12 @@ public class student {
 	int ID;
 	String name;
 	String course;
+	
+	public student(int Id,String name,String course) {
+		this.ID=Id;
+		this.name = name;
+		this.course=course;
+	}
 	public void setID(int iD) {
 		ID = iD;
 	}
@@ -21,9 +27,11 @@ public class student {
 	}
 	public String getCourse() {
 		return course;
+	}
+	@Override
+	public String toString() {
+		return "student ID=" + ID +"\n"+ "name=" + name +"\n"+ "course=" + course + " ";
 	}	
-
-
 }
 
 	
